44ded59316489d29ffe406e626640e8e6da25a0d
Goukl <gokulprocoder@gmail.com>
2024-11-05 00:49:54 +0530

    Removed avatar images

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,10 +24,10 @@ export const configData: Config = {
 	siteTitle: 'Foxi. Tailwind CSS Astro Starter Kit by Oxygenna',
 	siteDescription:
 		'Foxi is a design and development agency that specializes in creating beautiful and functional websites.',
-	ogImage: '/og.jpg',
+	ogImage: '/logo.svg',
 	logo: {
 		src: '/logo.svg',
-		alt: 'Foxi. logo'
+		alt: 'OnePoint. logo'
 	},
 	canonical: true,
 	noindex: false,
