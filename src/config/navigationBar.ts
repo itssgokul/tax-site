c511e16b1341fba7f995a47e70d2f070a65c9f7e
Goukl <gokulprocoder@gmail.com>
2024-11-05 00:49:54 +0530

    Removed avatar images

--- src/config/navigationBar.ts
+++ src/config/navigationBar.ts
@@ -35,7 +35,7 @@ export const navigationBarData: NavData = {
 	logo: {
 		src: '/logo.svg',
 		alt: 'The tailwind astro theme',
-		text: 'One Point.'
+		text: 'OnePoint.'
 	},
 	navItems: [
 		{ name: 'Home', link: '/' },
