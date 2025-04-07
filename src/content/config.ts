6e61b5a5652ff321d422a726a4f604883a450fde
Aadhann Rajesh <rajeshtrk1986@gmail.com>
2025-04-08 03:17:03 +0530

    Blog post updated

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
 import { z, defineCollection } from 'astro:content'
 
 const blogCollection = defineCollection({
-	schema: () =>
-		z.object({
+	schema: z.object({ // Removed function wrapper () => ...
 			title: z.string(),
 			description: z.string(),
 			pubDate: z.date(),
 			image: z.string(),
 			author: z.string(),
 			tags: z.array(z.string())
-		})
+		}), // Removed function wrapper () => ...
 })
 
 export const collections = {
