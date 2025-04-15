9383c411149139d5d111c538c554077be6a7c6e2
Aadhann Rajesh <rajeshtrk1986@gmail.com>
2025-04-15 06:32:45 +0530

    Button issue fixed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { z, defineCollection } from 'astro:content'
+import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
-	schema: z.object({ // Removed function wrapper () => ...
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.date(),
-			image: z.string(),
-			author: z.string(),
-			tags: z.array(z.string())
-		}), // Removed function wrapper () => ...
-})
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		image: z.string(),
+		author: z.string(),
+		tags: z.array(z.string()),
+	}),
+});
 
 export const collections = {
-	posts: blogCollection
-}
+	blog: blogCollection, // ✅ Use the defined collection
+};
