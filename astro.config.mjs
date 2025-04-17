aaa10a61be47cc30d293b93bea2db6d96c8bb6b7
Aadhann Rajesh <rajeshtrk1986@gmail.com>
2025-04-17 11:10:54 +0530

    adapter issue fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import netlify from "@astrojs/netlify"; // Import the Netlify adapter
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Enable server-side rendering for API routes
+  output: 'server',
+  adapter: netlify(), // Add the Netlify adapter
   site: "https://onepointtax.in/",
   integrations: [
     tailwind(),
