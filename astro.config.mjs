8a5b2e7cc4618ddef5631ab6bad90b0cbfaf3961
Aadhann Rajesh <rajeshtrk1986@gmail.com>
2025-04-17 11:13:39 +0530

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import netlify from "@astrojs/netlify"; // Import the Netlify adapter
-
+import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -11,6 +10,9 @@ export default defineConfig({
   output: 'server',
   adapter: netlify(), // Add the Netlify adapter
   site: "https://onepointtax.in/",
+  experimental: {
+    session: true, // ✅ Enable session support
+  },
   integrations: [
     tailwind(),
     icon(),
